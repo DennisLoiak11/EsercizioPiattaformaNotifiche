88b9c0a9c486170d28cbbdb33e6d0ee914ccd624
Dennis De Biasi <dennisloiak11@gmail.com>
2025-07-09 11:05:27 +0200

    classe NotificationManager

---
+++ NotificationManager.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotificationManager {
+    //istanza privata e statica della classe NotificationManager
+    private static NotificationManager instance;
+
+    //lista di utenti registrati (Observer)
+    private List<Observer> observers;
+
+    //costruttore privato per sovrascrivere il costruttore di default di java e impedire la creazione di istanze esterne alla classe
+    private NotificationManager() {
+        observers = new ArrayList<>();
+    }
+
+    //metodo pubblico statico per ottenere l’unica istanza
+    public static NotificationManager getInstance() {
+        if (instance == null) {
+            instance = new NotificationManager();
+        }
+        return instance;
+    }
+
+    //metodo per registrare un osservatore (utente)
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    //metodo per rimuovere un osservatore (utente)
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    //metodo per inviare notifiche a tutti gli osservatori
+    public void inviaNotifica(String messaggio) {
+        for (Observer observer : observers) {
+            observer.update(messaggio);
+        }
+    }
+}
