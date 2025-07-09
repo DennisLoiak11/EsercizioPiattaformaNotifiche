b0cd0d3c35f7b72c56661f789c0cb6a2536d5b01
Davide Latina <davide.latina616@gmail.com>
2025-07-09 14:53:00 +0200

    Update Utente.java

--- Utente.java
+++ Utente.java
@@ -3,14 +3,20 @@ interface Observer {
   void aggiorna(Messaggio msg);
 }
 
-public class Utente implements Observer {
+public class Utente extends DecoratoreMessaggioAstratto implements Observer {
 
   String nome;
   //Messaggio tipoMessaggio;
 
-  Utente(String nome/*, Messaggio tipoMessaggio*/) {
+  Utente(String nome, Messaggio messaggio) {
+    super(messaggio);
     this.nome = nome;
     //this.tipoMessaggio = tipoMessaggio;
+  }
+
+  @Override
+  public String getMessaggio() {
+    return messaggio.getMessaggio();
   }
 
   public void aggiorna(Messaggio msg) {
@@ -21,5 +27,23 @@ public class Utente implements Observer {
 
 
 }
+
+/*
+Public abstract class UserDecorator implements Observer {
+    protected Observer utenteDecorato;
+
+    public UserDecorator(Observer utente) {
+        this.utenteDecorato = utente;
+    }
+
+    public abstract void update(String messaggio);  // da implementare nei sottotipi
+}
+- Concetti:
+È un decoratore astratto.
+
+Riceve un oggetto Observer (es: User) da decorare.
+
+Permette di estendere dinamicamente il comportamento di update().
+ */
 
 
